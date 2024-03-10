8d182075708b32c02f35df8841ffd5a0a125be9c
shubham <shubhbhatt101@gmail.com>
2024-03-10 19:31:06 +0530

    cmmt

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
-app.listen(4000)
+mongoose.connect("mongodb+srv://shubhbhatt101:Shubham123@cluster0.x4lra5n.mongodb.net/")
+  .then(() => {
+    app.listen(4000)
+    console.log("connected to db")
+  })
+  .catch((error) => {
+    console.log('error connecting to database',error)
+  })
 
-app.get('/', (req,res)=>{
+
+
+app.get('/', (req, res) => {
   const blogs = [
-    {title: "Fallout 4", snippet: 'lorem impsum dipsum lorem impsum dipsum'},
-    {title: "Metro Exodus", snippet: 'lorem impsum dipsum lorem impsum dipsum'},
-    {title: "Disohnered 2", snippet: 'lorem impsum dipsum lorem impsum dipsum'},
-    {title: "Far Cry 5", snippet: 'lorem impsum dipsum lorem impsum dipsum'},
+    { title: "Fallout 4", snippet: 'lorem impsum dipsum lorem impsum dipsum' },
+    { title: "Metro Exodus", snippet: 'lorem impsum dipsum lorem impsum dipsum' },
+    { title: "Disohnered 2", snippet: 'lorem impsum dipsum lorem impsum dipsum' },
+    { title: "Far Cry 5", snippet: 'lorem impsum dipsum lorem impsum dipsum' },
   ]
-    res.render("home",{title:'Home', blogs})
+  res.render("home", { title: 'Home', blogs })
 })
-app.get('/about', (req, res)=>{
-  res.render('about', {title:'About'})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' })
 })
 
 
-app.get('/about/form', (req,res)=>{
+app.get('/about/form', (req, res) => {
   res.render('form')
 })
 
@@ -31,6 +41,6 @@ app.get('/about/form', (req,res)=>{
 //     res.redirect('/about')
 // })
 
-app.use((req,res)=>{
-    res.status(404).render('404', {title:'404'})
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' })
 })
